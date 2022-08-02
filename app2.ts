039c9fcecf93b999c07f322a7254169e74079f95
NJL <nijialun888@hotmail.com>
2022-08-02 20:51:09 +0800

    format

--- app2.ts
+++ app2.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 5555;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post('',() =>{})
+app.post("", () => {});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
